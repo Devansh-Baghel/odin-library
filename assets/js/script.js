84abcd1dac3fd0ded6452bf83616c46f63e6c574
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-05-31 21:23:43 +0530

    Add js to show hide modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
-// This is just to test how all cards would look, to be removed later
 const addBookButton = document.querySelector(".add-book");
 const cardsWrapper = document.querySelector(".cards");
 
-function addCard(){
-	const newCard = document.createElement("div");
-	cardsWrapper.appendChild(newCard);
-	newCard.classList.add("card");
+const modal = document.querySelector("dialog");
+
+const titleInput = document.querySelector("#title-input");
+const authorInput = document.querySelector("#author-input");
+const submitButton = document.querySelector(".submit-button");
+
+// function addCard(){
+// 	const newCard = document.createElement("div");
+// 	cardsWrapper.appendChild(newCard);
+// 	newCard.classList.add("card");
+// }
+
+// addBookButton.addEventListener("click", addCard);
+addBookButton.addEventListener("click", () => {
+	modal.showModal();
+})
+
+
+let library = [];
+
+function Book (title, author){
+	this.title = title;
+	this.author = author;
 }
 
-addBookButton.addEventListener("click", addCard);
+function addBookToLibrary() {
+	
+}
+
+
+submitButton.addEventListener("click", addBookToLibrary);
